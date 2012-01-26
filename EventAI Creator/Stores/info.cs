17786b16df278f09322c146a7c7b0585c1c87444
Xfurry <xfurry@scriptdev2.com>
2012-01-27 01:00:36 +0200

    Update with the latest core events and actions

--- EventAI Creator/Stores/info.cs
+++ EventAI Creator/Stores/info.cs
@@ -11,7 +11,8 @@ namespace EventAI_Creator
 
     static class Info
     {
-        public static string[,] EventListInfo = new string[,]{
+        public static string[,] EventListInfo = new string[,]
+        {
 /*00 | 01*/{"TIMER","InitialMin","InitialMax","RepeatMin","RepeatMax","Expires first between (Param1) and (Param2) and then between every (Param3) and (Param4).  but only in combat."},
 /*01 | 02*/{"TIMER_OOC","InitialMin","InitialMax","RepeatMin","RepeatMax","Expires first between (Param1) and (Param2) and then between every (Param3) and (Param4).  but only out of combat."},
 /*02 | 03*/{"HP","HPMax%","HPMin%","RepeatMin","RepeatMax ","Expires when HP is between (Param1) and (Param2). Will repeat every (Param3) and (Param4)."},
@@ -29,10 +30,22 @@ namespace EventAI_Creator
 /*14 | 15*/{"FRIENDLY_HP","HPDeficit","Radius","RepeatMin","RepeatMax","Expires when a friendly unit in radius has at least (param1) hp missing. Will repeat every (Param3) and (Param4) ."},
 /*15 | 16*/{"FRIENDLY_IS_CC","DispelType","Radius","RepeatMin","RepeatMax","Expires when a friendly unit is Crowd controlled within the given radius (param2). Will repeat every (Param3) and (Param4) ."},
 /*16 | 17*/{"MISSING_BUFF","SpellId","Radius","RepeatMin","RepeatMax","Expires when a friendly unit is missing aura's given by spell (param1) within radius (param2). Will repeat every (Param3) and (Param4) ."},
-/*17 | 18*/{"SUMMONED_UNIT","CreatureId","RepeatMin","RepeatMax","","Expires after creature with entry = (param1) is spawned or for all spawns if param1 = 0. Will repeat every (Param2) and (Param3) ."}
+/*17 | 18*/{"SUMMONED_UNIT","CreatureId","RepeatMin","RepeatMax","","Expires after creature with entry = (param1) is spawned or for all spawns if param1 = 0. Will repeat every (Param2) and (Param3) ."},
+/*18 | 19*/{"TARGET_MANA","ManaMax%","ManaMin%","RepeatMin","RepeatMax","Expires when current target's Mana is between (Param1) and (Param2). Will repeat every (Param3) and (Param4)."},
+/*19 | 20*/{"QUEST_ACCEPT","","","","","Event not implemented in core - use at your own risk"},
+/*20 | 21*/{"QUEST_COMPLETE","","","","","Event not implemented in core - use at your own risk"},
+/*21 | 22*/{"REACHED_HOME","","","","","Expires when a creature reaches it's home (spawn) location after evade."},
+/*22 | 23*/{"RECEIVE_EMOTE","EmoteId","Condition","CondValue1","CondValue2","Expires when a creature receives an emote with emote text id (enum TextEmotes) in (Param1). Conditions can be defined (Param2) with optional values (Param3,Param4), see enum ConditionType."},
+/*23 | 24*/{"AURA","SpellID","AmmountInStack","RepeatMin","RepeatMax","Expires when a creature has spell (Param1) auras applied in a stack greater or equal to value provided in (Param2). Will repeat every (Param3) and (Param4)."},
+/*24 | 25*/{"TARGET_AURA","SpellID","AmmountInStack","RepeatMin","RepeatMax","Expires when the current target unit has spell (Param1) auras applied in a stack greater or equal to value provided in (Param2). Will repeat every (Param3) and (Param4)."},
+/*25 | 26*/{"SUMMONED_JUST_DIED","CreatureId","RepeatMin","RepeatMax","","Expires after creature with entry = (Param1) is die (Param1 = 0 means all spawns). Will repeat every (Param2) and (Param3)."},
+/*26 | 27*/{"SUMMONED_JUST_DESPAWN","CreatureId","RepeatMin","RepeatMax","","Expires before creature with entry = (Param1) is despawn (Param1 = 0 means all spawns). Will repeat every (Param2) and (Param3)."},
+/*27 | 28*/{"MISSING_AURA","SpellID","AmmountInStack","RepeatMin","RepeatMax","Expires when a creature not has spell (Param1) auras applied in a stack greater or equal to value provided in (Param2). Will repeat every (Param3) and (Param4)."},
+/*28 | 29*/{"TARGET_MISSING_AURA","SpellID","AmmountInStack","RepeatMin","RepeatMax","Expires when when the current target unit not has spell (Param1) auras applied in a stack greater or equal to value provided in (Param2). Will repeat every (Param3) and (Param4)."}
        };
 
-        public static string[,] ActionListInfo = new string[,]{
+        public static string[,] ActionListInfo = new string[,]
+        {
 /*00 | 01 */{"NONE","","","","Does nothign"},
 /*01 | 01 */{"SAY","TextId","","","Says text"},
 /*02 | 01 */{"YELL","TextId","","","Yellls text"},
@@ -71,7 +84,12 @@ namespace EventAI_Creator
 /*35 | 01 */{"SET_INST_DATA64","Field","Target","","Calls ScriptedInstance::SetData64 with field (param1) and data (param2) target's GUID."},
 /*36 | 01 */{"UPDATE_TEMPLATE","TemplateId","Team","","Changes the creature to a new creature template of (param1) with team = Alliance if (param2) = false or Horde if (param2) = true"},
 /*37 | 01 */{"DIE","","","","Kills the creature"},
-/*38 | 01 */{"ZONE_COMBAT_PULSE","","","","Places all players within the instance into combat with the creature. Only works in combat and only works inside of instances."}
+/*38 | 01 */{"ZONE_COMBAT_PULSE","","","","Places all players within the instance into combat with the creature. Only works in combat and only works inside of instances."},
+/*39 | 01 */{"CALL_FOR_HELP","Radius","","","Call any friendly out-of-combat creatures in a radius (Param1) to attack current creature's target."},
+/*40 | 01 */{"SET_SHEATH","Sheath","","","Sets sheath state for a creature (0 = no weapon, 1 = melee weapon, 2 = ranged weapon)."},
+/*41 | 01 */{"FORCE_DESPAWN","Delay","","","Despawns the creature, if delay = 0 immediate otherwise will despawn after delay time set in Param1 (in ms)."},
+/*42 | 01 */{"SET_INVINCIBILITY_HP_LEVEL","HP_Level","HP_Percent","","Set min. health level for creature that can be set at damage as flat value or percent from max health"},
+/*43 | 01 */{"MOUNT_TO_ENTRY_OR_MODEL","CreatureEntry","ModelId","","Set mount model from creature_template.entry (Param1) OR explicit modelId (Param2). If (Param1) AND (Param2) are both 0, unmount."}
         };
     }
 }
